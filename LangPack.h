4b255e560a23b0527a291faa68388d38408d71cc
5N62V <n_dmitrij@ukr.net>
2018-01-29 09:19:01 +0200

    Create LangPack.h

---
+++ LangPack.h
@@ -0,0 +1,45 @@
+const char str_0[] PROGMEM = "Для выхода - SELECT";
+const char str_1[] PROGMEM = "ЗАДЕРЖКА ВКЛЮЧЕНИЯ";
+const char str_2[] PROGMEM = "ДО СВИДАНИЯ!";
+const char str_3[] PROGMEM = "ГРОМКОСТЬ";
+const char str_4[] PROGMEM = "БАЛАНС";
+const char str_5[] PROGMEM = "Вход:";
+const char str_6[] PROGMEM = "Выход: ВКЛ.";
+const char str_7[] PROGMEM  = "Вернуться";
+const char str_8[] PROGMEM  = "Селектор Входов";
+const char str_9[] PROGMEM  = "Баланс";
+const char str_10[] PROGMEM  = "Темброблок";
+const char str_11[] PROGMEM  = "Задержка Вкл.";//
+const char str_12[] PROGMEM  = "Пункт Меню 1";
+const char str_13[] PROGMEM  = "Пункт Меню 2";
+const char str_14[] PROGMEM  = "Вход 1";
+const char str_15[] PROGMEM  = "Вход 2";
+const char str_16[] PROGMEM  = "Вход 3";
+const char str_17[] PROGMEM  = "USB";
+const char str_18[] PROGMEM  = "Cекунд";
+const char str_19[] PROGMEM  = "СЕЛЕКТОР ВХОДОВ";
+const char str_20[] PROGMEM  = "ТЕМБРОБЛОК";
+const char str_21[] PROGMEM  = "Вернуться";
+const char str_22[] PROGMEM  = "Включить";
+const char str_23[] PROGMEM  = "Настроить";
+const char str_24[] PROGMEM  = "Выключить";
+const char str_25[] PROGMEM  = "ВКЛЮЧЕН";
+const char str_26[] PROGMEM  = "ВЫКЛЮЧЕН";
+const char str_27[] PROGMEM  = "Вернуться";
+const char str_28[] PROGMEM  = "Низкие";
+const char str_29[] PROGMEM  = "Высокие";
+const char str_30[] PROGMEM  = "НАСТРОЙКА ТЕМБРОБЛОКА";
+const char str_31[] PROGMEM  = "-6dB";
+const char str_32[] PROGMEM  = "-4dB";
+const char str_33[] PROGMEM  = "-2dB";
+const char str_34[] PROGMEM  = " 0dB";
+const char str_35[] PROGMEM  = "+2dB";
+const char str_36[] PROGMEM  = "+4dB";
+const char str_37[] PROGMEM  = "+6dB";
+
+const char* const text[] PROGMEM = {str_0, str_1, str_2, str_3, str_4, str_5, str_6,
+                                    str_7, str_8, str_9, str_10, str_11, str_12, str_13,
+                                    str_14, str_15, str_16, str_17, str_18, str_19, str_20,
+                                    str_21, str_22, str_23, str_24, str_25, str_26, str_27, 
+                                    str_28, str_29, str_30, str_31, str_32, str_33, str_34,
+                                    str_35, str_36, str_37};
